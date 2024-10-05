061be6a88c7a1f5752da8f0a75c32f43988a6db6
El-Pajas-XD <hacksore515@gmail.com>
2024-10-04 21:51:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("nameForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var name = document.getElementById("name").value;
-    document.getElementById("greeting").textContent = "Nombre confirmado:, " + name + ", Ya puedes irse 😎";
+    document.getElementById("greeting").textContent = "Nombre confirmado: " + name + ", Ya puedes irse 😎";
 });
 
